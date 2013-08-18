9b60775193696dad32509030c6389d0015ee3e9c
klay2000 <motoklay@gmail.com>
2013-08-18 12:53:22 -0400

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -5,8 +5,33 @@ using namespace std;
 
 int main()
 {
-int hp;
-int enemyhp;
-int defense;
-int enemydefense;
+//is stands for in sight (os is opposite), line of sight is 25 characters of text starting with one and adding two with each character further forward
+
+//in sight NPC stats
+int hpis [25];
+int defenseis [25];
+int staminais [25];
+int attackis [25];
+int strengthis [25];
+int inventoryis [25] [10];
+
+//out of sight NPC stats (can run fifty NPCs at one time)
+int int hpos [50];
+int defenseos [50];
+int staminaos [50];
+int attackos [50];
+int strengthos [50];
+int inventoryos [50] [10];
+
+//player stats
+string playername;
+int hpplayer;
+int defenseplayer;
+int staminaplayer;
+int attackplayer;
+int strengthplayer;
+int inventoryplayer [10];
+
+char map [10] [10] = {0};
+
 }
