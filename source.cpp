38a8560a27eba7fa036e09f0d23856b62b028d3b
klay2000 <motoklay@gmail.com>
2013-08-21 01:39:23 -0400

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,3 +1,51 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+int main()
+{
+//is stands for in sight (os is opposite), line of sight is 25 characters of text starting with one and adding two with each character further forward
+
+
+//in sight NPC stats
+int hpis [25];
+int defenseis [25];
+int staminais [25];
+int attackis [25];
+int strengthis [25];
+int inventoryis [25] [10];
+
+
+//out of sight NPC stats (can run fifty NPCs at one time)
+int hpos [50];
+int defenseos [50];
+int staminaos [50];
+int attackos [50];
+int strengthos [50];
+int inventoryos [50] [10];
+
+
+int npcx [50];
+int npcy [50];
+
+
+//player stats
+string playername;
+int hpplayer;
+int defenseplayer;
+int staminaplayer;
+int attackplayer;
+int strengthplayer;
+int inventoryplayer [10];
+string command;
+
+//READ THIS x and y are reversed in the graphics sorry :(
+
+int x = 1;
+int y = 1;
 //prev = previous
 int prevx;
 int prevy;
@@ -36,7 +84,7 @@ if(command == "s")y++;
 if(command == "d")x++;
 if(command == "exit")gameon = 0;
 
-if(map[x] [y] != 0){
+if(map[y] [x] != 0){
 x = prevx;
 y = prevy;
 }
