b6bb7449132cca6b7964eae62654e7472c7f405c
OmegaPrecursor <cyberfluxing@gmail.com>
2013-08-23 08:18:39 +0100

    Update source.cpp
    Edited system("cls") to a function which wipes the screen without using the infamous system() functions, which can cause security issues.

--- source.cpp
+++ source.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
 #include <string>
-
+#include <windows.h>
 
 using namespace std;
 
+void ClearScreen()
+  {
+  HANDLE                     hStdOut;
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+  DWORD                      count;
+  DWORD                      cellCount;
+  COORD                      homeCoords = { 0, 0 };
+
+  hStdOut = GetStdHandle( STD_OUTPUT_HANDLE );
+  if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+  /* Get the number of cells in the current buffer */
+  if (!GetConsoleScreenBufferInfo( hStdOut, &csbi )) return;
+  cellCount = csbi.dwSize.X *csbi.dwSize.Y;
+
+  /* Fill the entire buffer with spaces */
+  if (!FillConsoleOutputCharacter(
+    hStdOut,
+    (TCHAR) ' ',
+    cellCount,
+    homeCoords,
+    &count
+    )) return;
+
+  /* Fill the entire buffer with the current colors and attributes */
+  if (!FillConsoleOutputAttribute(
+    hStdOut,
+    csbi.wAttributes,
+    cellCount,
+    homeCoords,
+    &count
+    )) return;
+
+  /* Move the cursor home */
+  SetConsoleCursorPosition( hStdOut, homeCoords );
+  }
 
 int main()
 {
@@ -91,7 +127,7 @@ x = prevx;
 y = prevy;
 }
 
-cin.get(); 
+ClearScreen(); // Successfully clear the sreen without using system dependencies.
 
 map [prevy] [prevx] = 0;
 }
